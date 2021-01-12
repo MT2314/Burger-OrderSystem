06a45b9e38c8548b4761ca1e1270195cacd67035
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-01-11 19:57:04 -0500

    MYSQL Schema and Seeder files Complete

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobella Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+
