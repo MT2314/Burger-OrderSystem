869f5f217b79c7935114690a07dc76c8c335f8ab
MT2314 <39105763+MT2314@users.noreply.github.com>
2021-01-14 05:51:17 -0500

    mysql JawsConnection

--- models/config/connection.js
+++ models/config/connection.js
@@ -5,19 +5,20 @@ if(process.env.JAWSDB_URL) {
 }
 else{
   connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'mikeServer1!',
-    database: 'burgers_db'
+    host: 'lfmerukkeiac5y5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'cbrxr4wvkfv4baid',
+    password: 'vwwrj0squ4mx69y2!',
+    database: 'o0effs2ujic95mna'
   });
 };
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'mikeServer1!',
-  database: 'burgers_db',
-});
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//   password: 'mikeServer1!',
+//   database: 'burgers_db',
+// });
 
 // Make connection.
 connection.connect((err) => {
